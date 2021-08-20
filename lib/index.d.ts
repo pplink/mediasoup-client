776569ff4e4960f5987ba6a63d35fa4a8cf370a8
geongun20 <leesg9@snu.ac.kr>
2021-08-20 21:16:50 +0900

    3.6.37-pplink1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -8,7 +8,7 @@ export { types };
 /**
  * Expose mediasoup-client version.
  */
-export declare const version = "3.6.37-logger";
+export declare const version = "3.6.37-pplink0";
 /**
  * Expose Device class and detectDevice() helper.
  */
